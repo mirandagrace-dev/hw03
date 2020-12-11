ec31a7e2dfeb99607fd554a373c0c353a6931ea9
Miranda McDonald <mirandagrace.dev@gmail.com>
2020-12-10 20:13:46 -0500

    added edge case for length and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hw03
+
+## Description
+
+This web application's purpose is to provide the user a randomly generated password based on critera asked by the website. As the user goes through the prompts for the password criteria, the data is stored and then used to create the new password.
+
+---
+
+​
+© 2020 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved
--- script.js
+++ script.js
@@ -20,40 +20,45 @@ function writePassword() {
 function generatePassword() {
     //prompt user
     pwLength = prompt("How many characters do you want the password to be?");
-    pwNum = confirm("Do you want to allow numeric characters?");
-    pwCap = confirm("Do you want to allow capital letters?");
-    pwLow = confirm("What about lowercase ones?");
-    pwSpChar = confirm("And finally, do you want to include special characters?");
-
-    //create local variables
-    var criteria = [];
-    var newPW = "";
-
-    //check which criteria the user selected
-    if(pwNum){
-        criteria.push('1','2','3','4','5','6','7','8','9','0');
-    }
-    if(pwCap){
-        criteria.push('A','B','C','D','E','F','G','H','I','J','K','L'
-            ,'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-    }
-    if(pwLow){
-        criteria.push('a','b','c','d','e','f','g','h','i','j','k','l'
-            ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    }
-    if(pwSpChar){
-        criteria.push('!','#','$','%','&','(',')','-','/',':',';','?','@','[',']','~');
-    }
-
-    //loop through length of desired password and add a random character from the criteria array
-    for(var i = 0; i < pwLength; i++){
-        var randomChar = criteria[Math.floor(criteria.length * Math.random())];
-
-        //add new character to the new password string
-        newPW = newPW.concat(randomChar);
-    }
     
-    return newPW;
+    if(pwLength >=8 && pwLength <= 128){
+        pwNum = confirm("Do you want to allow numeric characters?");
+        pwCap = confirm("Do you want to allow capital letters?");
+        pwLow = confirm("What about lowercase ones?");
+        pwSpChar = confirm("And finally, do you want to include special characters?");
+
+        //create local variables
+        var criteria = [];
+        var newPW = "";
+
+        //check which criteria the user selected
+        if(pwNum){
+            criteria.push('1','2','3','4','5','6','7','8','9','0');
+        }
+        if(pwCap){
+            criteria.push('A','B','C','D','E','F','G','H','I','J','K','L'
+                ,'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+        }
+        if(pwLow){
+            criteria.push('a','b','c','d','e','f','g','h','i','j','k','l'
+                ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+        }
+        if(pwSpChar){
+            criteria.push('!','#','$','%','&','(',')','-','/',':',';','?','@','[',']','~');
+        }
+
+        //loop through length of desired password and add a random character from the criteria array
+        for(var i = 0; i < pwLength; i++){
+            var randomChar = criteria[Math.floor(criteria.length * Math.random())];
+
+            //add new character to the new password string
+            newPW = newPW.concat(randomChar);
+        }
+
+        return newPW;
+    }
+    else {alert("Password length can only be 8-128 characters. Please try again.")}
+    
 }
 
 // Add event listener to generate button
